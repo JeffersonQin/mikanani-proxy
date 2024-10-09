02f02a343b2412936c2c8ed526ea424313b0e4e7
轻尘 <53001305+SlightDust@users.noreply.github.com>
2024-10-10 01:51:04 +0800

    :sparkles:修改pubDate格式为RFC2822并移动到item下

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import uvicorn
 from fastapi import FastAPI, Response
 from lxml import etree
+from datetime import datetime
 
 run_host = "0.0.0.0"
 run_port = 9115
@@ -33,6 +34,31 @@ def root(token: str):
             f"{host_name}/get?token={user_token}&link=https://mikanani.me",
         )
         link.set("url", url)
+    
+    # 修改pubdate格式
+    namespaces = {'ns': 'https://mikanani.me/0.1/'}
+    for item in doc.findall('.//item'):
+        # 使用find找到torrent节点，再找到pubDate
+        pub_date_element = item.find('.//ns:torrent/ns:pubDate', namespaces)
+        if pub_date_element is not None:
+            # 原始pubDate字符串
+            pub_date_str = pub_date_element.text
+            # 去掉微秒部分，转换格式
+            if '.' in pub_date_str:
+                pub_date_str = pub_date_str.split('.')[0]  # 去掉微秒部分
+            # 解析原始日期字符串
+            pub_date = datetime.fromisoformat(pub_date_str)
+            # 将日期格式化为目标格式
+            formatted_pub_date = pub_date.strftime('%a, %d %b %Y %H:%M:%S +0800')
+            # 更新pubDate元素的文本
+            # pub_date_element.text = formatted_pub_date  # 也没必要
+
+            # 将pubDate元素移动到item下
+            # 创建新的pubDate节点
+            new_pub_date_element = etree.Element('pubDate')
+            new_pub_date_element.text = formatted_pub_date
+            # 将新的pubDate添加到item的最后
+            item.append(new_pub_date_element)
 
     content = etree.tostring(doc)
     return Response(content, media_type="application/xml")
